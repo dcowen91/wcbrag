29f078a097de7c56f1da1a7d489f45bf6f7d7b5f
Drew Owen <dcowen@email.arizona.edu>
2014-06-21 15:54:20 -0700

    added basic functionality

---
+++ app/app.js
@@ -0,0 +1,126 @@
+var players;
+var headings;
+$(document).ready(function () {
+	console.log("ready");
+	loadCSV();
+
+});
+
+function loadCSV() {
+	$.ajax({
+        type: "GET",
+        url: "/app/data.csv",
+        dataType: "text",
+        success: function(data) {
+        	processCSV(data);
+        }
+     });
+}
+
+function processCSV(allText) {
+	var lines = allText.split("\n");
+	headings = lines[0].split(",");
+	headings.pop(); //remove empty string
+	lines.splice(0, 1); //remove headings from array
+	var Tplayers = [];
+	$.each(lines, function(index, value) {
+		var line = value.split(",");
+		var player = {};
+		$.each(headings, function(Hi, Hv) {
+			player[Hv] = line[Hi];  //populate object with KV pairings from csv
+		})
+		Tplayers.push(player);
+	});
+	players = $.grep(Tplayers, function(e) {
+		return e.Goals > 0;
+	});
+
+}
+
+$('#teams').click(function() {
+	$('li').removeClass('active');
+	$('#teams').parent().addClass('active');
+	displayTeams();
+});
+
+$('#players').click(function() {
+	$('li').removeClass('active');
+	$('#players').parent().addClass('active');
+	displayPlayers();
+});
+
+$('#leagues').click(function() {
+	$('li').removeClass('active');
+	$('#leagues').parent().addClass('active');
+	displayLeagues();
+});
+
+$('#countries').click(function() {
+	$('li').removeClass('active');
+	$('#countries').parent().addClass('active');
+	displayCountries();
+});
+
+$('#about').click(function() {
+	$('li').removeClass('active');
+	$('#about').parent().addClass('active');
+	displayAbout();
+});
+
+function displayAbout() {
+	$('#main').html("");
+	$('#main').append("<h1>WCBrag</h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae arcu id odio eleifend facilisis. Praesent quis mi et erat luctus aliquam in vitae arcu. Nam sit amet convallis libero. Quisque tincidunt congue ligula eu ornare. Donec blandit arcu ut felis viverra sodales. Vivamus tincidunt ultricies justo sed aliquet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse vehicula aliquam nibh quis blandit. Curabitur egestas sem leo, ut congue erat dapibus a. In quis vulputate ipsum. Nunc ut lobortis nulla, eu lacinia orci. Vivamus fringilla facilisis massa auctor auctor. Curabitur id fermentum enim, vel fermentum.");
+}
+
+
+function displayPlayers() {
+	$('#main').html("");
+	$.each(players, function(i, val) {
+		$('#main').append("<div class='row panel panel-default'> <div class='panel-body'><div class='col-md-3'> <p>" +val.Player + "</p></div> <div class='col-md-3'><p>" + val.Goals + "</p></div> <div class='col-md-3'><p>" + val.Club + "</p></div> <div class='col-md-3'><p>" + val.Country + "</p></div> </div> </div>");
+	});
+}
+
+function displayTeams() {
+	$('#main').html("");
+	var teams = {};
+	$.each(players, function(i, v) { 
+		if (!teams[v.Club]) {
+    		teams[v.Club] = 0;
+		}
+		teams[v.Club] += parseInt(v.Goals);
+	});
+	teamSorted = Object.keys(teams).sort(function(a,b){return teams[b]-teams[a]});
+	$.each(teamSorted, function(i, v) {
+		$('#main').append("<div class='row panel panel-default'> <div class='panel-body'><div class='col-md-11'> <p>" + v + "</p></div> <div class='col-md-1'><p>" + teams[v] + "</p></div> </div> </div>");
+	});
+}
+
+function displayLeagues() {
+	$('#main').html("");
+	var leagues = {};
+	$.each(players, function(i, v) { 
+		if (!leagues[v.League]) {
+    		leagues[v.League] = 0;
+		}
+		leagues[v.League] += parseInt(v.Goals);
+	});
+	leagueSorted = Object.keys(leagues).sort(function(a,b){return leagues[b]-leagues[a]});
+	$.each(leagueSorted, function(i, v) {
+		$('#main').append("<div class='row panel panel-default'> <div class='panel-body'><div class='col-md-11'> <p>" + v + "</p></div> <div class='col-md-1'><p>" + leagues[v] + "</p></div> </div> </div>");
+	});
+}
+
+function displayCountries() {
+	$('#main').html("");
+	var countries = {};
+	$.each(players, function(i, v) { 
+		if (!countries[v.Country]) {
+    		countries[v.Country] = 0;
+		}
+		countries[v.Country] += parseInt(v.Goals);
+	});
+	leagueSorted = Object.keys(countries).sort(function(a,b){return countries[b]-countries[a]});
+	$.each(leagueSorted, function(i, v) {
+		$('#main').append("<div class='row panel panel-default'> <div class='panel-body'><div class='col-md-11'> <p>" + v + "</p></div> <div class='col-md-1'><p>" + countries[v] + "</p></div> </div> </div>");
+	});
+}
--- index.php
+++ index.php
@@ -1,5 +1,48 @@
-<?php
-
-echo "Hello World!";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WCBrag</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="WCBrag: compare leagues' and teams' performances at the World Cup!">
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="app/app.css">
+</head>
+<body>
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand">WCBrag</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="#/players" id='players'>Players</a></li>
+					<li><a href="#/teams" id='teams'>Teams</a></li>
+					<li><a href="#/leagues" id='leagues'>Leagues</a></li>
+					<li><a href="#/countries" id='countries'>Countries</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#/about" id='about'>About</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container -->
+	</nav>
+	<div class="container">
+		<div id = 'main' class='jumbotron'>
+			<h1>
+				WCBrag
+			</h1>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae arcu id odio eleifend facilisis. Praesent quis mi et erat luctus aliquam in vitae arcu. Nam sit amet convallis libero. Quisque tincidunt congue ligula eu ornare. Donec blandit arcu ut felis viverra sodales. Vivamus tincidunt ultricies justo sed aliquet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse vehicula aliquam nibh quis blandit. Curabitur egestas sem leo, ut congue erat dapibus a. In quis vulputate ipsum. Nunc ut lobortis nulla, eu lacinia orci. Vivamus fringilla facilisis massa auctor auctor. Curabitur id fermentum enim, vel fermentum.
+		</div>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" rel="script"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" rel="script"></script>
+	<script src="app/app.js"></script>
+</body>
+</html>
